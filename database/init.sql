35a67bb1e9287fe91af65b3a69839d47ae0c67d8
hank <hankphan.dev@gmail.com>
2024-07-07 15:52:58 +0700

    Update database

--- database/init.sql
+++ database/init.sql
@@ -1,9 +1,59 @@
-CREATE TABLE IF NOT EXISTS users
+CREATE TABLE users
 (
-    id  SERIAL PRIMARY KEY,
-    username VARCHAR(255) UNIQUE NOT NULL,
-    email    VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_DATE,
-    updated_at TIMESTAMP
+    user_id       UUID PRIMARY KEY,
+    username      VARCHAR(50)  NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    email         VARCHAR(100) NOT NULL UNIQUE,
+    created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE categories
+(
+    category_id UUID PRIMARY KEY,
+    name        VARCHAR(100) NOT NULL UNIQUE,
+    description TEXT
+);
+
+
+CREATE TABLE posts
+(
+    post_id     UUID PRIMARY KEY,
+    user_id     UUID,
+    title       VARCHAR(255) NOT NULL,
+    content     TEXT         NOT NULL,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    category_id UUID,
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
+    FOREIGN KEY (category_id) REFERENCES categories (category_id)
+);
+
+
+CREATE TABLE comments
+(
+    comment_id UUID PRIMARY KEY,
+    post_id    UUID,
+    user_id    UUID,
+    content    TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (post_id) REFERENCES posts (post_id),
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+
+CREATE TABLE tags
+(
+    tag_id UUID PRIMARY KEY,
+    name   VARCHAR(50) NOT NULL UNIQUE
+);
+
+
+CREATE TABLE post_tags
+(
+    post_id UUID,
+    tag_id  UUID,
+    PRIMARY KEY (post_id, tag_id),
+    FOREIGN KEY (post_id) REFERENCES posts (post_id),
+    FOREIGN KEY (tag_id) REFERENCES tags (tag_id)
 );
--- database/schema.sql
+++ database/schema.sql
@@ -1,5 +1,5 @@
-INSERT INTO users(username, email, password)
-VALUES ('user1', 'user1@gmail.com', 'user1'),
-       ('user2', 'user2@gmail.com', 'user2'),
-       ('user3', 'user3@gmail.com', 'user3');
+INSERT INTO users(user_id, username, email, password_hash)
+VALUES ('01908c5f-39db-7c51-bc7e-c5a5dd30351c', 'user1', 'user1@gmail.com', 'user1'),
+       ('01908c5f-6f88-7a35-a48f-72991b1f5c3f', 'user2', 'user2@gmail.com', 'user2'),
+       ('01908c5f-da0c-7d5f-8691-5248f81b2be1','user3', 'user3@gmail.com', 'user3');
 
