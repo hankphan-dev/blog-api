ef1aac41ae106a8864c116060f6bb6cf201cce43
hank <hankphan.dev@gmail.com>
2024-07-07 16:13:01 +0700

    Update user db

--- database/init.sql
+++ database/init.sql
@@ -2,7 +2,7 @@ CREATE TABLE users
 (
     user_id       UUID PRIMARY KEY,
     username      VARCHAR(50)  NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     email         VARCHAR(100) NOT NULL UNIQUE,
     created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,4 @@
-INSERT INTO users(user_id, username, email, password_hash)
+INSERT INTO users(user_id, username, email, password)
 VALUES ('01908c5f-39db-7c51-bc7e-c5a5dd30351c', 'user1', 'user1@gmail.com', 'user1'),
        ('01908c5f-6f88-7a35-a48f-72991b1f5c3f', 'user2', 'user2@gmail.com', 'user2'),
        ('01908c5f-da0c-7d5f-8691-5248f81b2be1','user3', 'user3@gmail.com', 'user3');
